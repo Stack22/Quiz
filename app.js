a3a6bc2476d17dad76a26a1c4fdf518666bba81c
Stack22 <stacyking1421@gmail.com>
2017-02-02 13:54:45 -0500

    add (very)basic psuedo code

---
+++ app.js
@@ -0,0 +1,25 @@
+// State
+var state = [
+  {
+    question: 0,
+    answer: "",
+    correct: 0,
+    incorrect: 0
+  }
+];
+
+// Create State
+
+// Read State
+
+// Update State
+
+// Delete State
+
+// Render
+
+// Handle
+
+// Event Listeners
+
+//
