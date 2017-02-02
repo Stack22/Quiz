6c56f87c238f7e45098d94e7711fe9e2ddc591d0
Stack22 <stacyking1421@gmail.com>
2017-02-02 14:50:56 -0500

    add psuedo code actions for functions

--- app.js
+++ app.js
@@ -10,73 +10,84 @@ var state = [
 
 // Create State
 function startState() {
-
+  // make question: 1 (or id string?)
+  // make answer: answer string
+  // make correct: 0
+  // make incorrect: 0
 };
 
 // Read State
 function readQuestionNum() {
-
+  // return current question id
 };
 
 function readAnswer() {
-
+  // return answer string
 };
 
 function readCorrect() {
-
+  // return correct current total
 };
 
 function readIncorrect() {
-
+  // return incorrect current total
 };
 
 // Update State
 function updateQuestionNum() {
-
+  // make current question id +1 (or update id string using loop int)
 };
 
 function updateAnswer() {
-
+  // make current answer = updated Q answer
 };
 
 function updateCorrect() {
-
+  // make correct total +1
 };
 
 function updateIncorrect() { // if/else?
-
+  // make incorrect total +1
 };
 
 // Delete State
 
 // Render
 function renderStart() {
-
+  // Show start view w/start button visible
 };
 
 function renderQuestion() {
-
+  // remove start view/start button
+  // replace with question view
 };
 
 function renderScore() {
-
+  // show score totals after 1st question Submit
 };
 
 function renderFinalScore() { // if/else?
-
+  // show final score after final question Submit
 };
+
+function renderButton() {
+  // toggle between Start, Submit, Next Question, Finish
+}
 
 // Handle
 function handleStart() {
-
+  // renderQuestion()
+  // renderButton()
 };
 
 function handleSubmit() {
-
+  // updateCorrect()
+  // updateIncorrect()
+  // renderButton()
 };
 
 function handleContinue() {
-
+  //
 };
 
 function handleFinish() { // if/else?
